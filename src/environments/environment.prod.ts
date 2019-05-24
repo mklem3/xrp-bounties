9a9c829d4732830d72e2357bc79b4094136302a4
Matthew Klemm <matthew_klemm@live.com>
2019-05-24 22:56:57 +1000

    Implement travis ci (#3)
    * angular fire integration + basic store test
    
    * untrack the envirenment.ts for safekeeping api key
    
    * update readme for config firebase
    
    * Added .travis.yml
    
    * limited to builds to master
    
    * added language definitions
    
    * added firebase hosting for ci
    
    * updates to travis file
    
    * Fixed incorrect formatting
    
    * only master branches will build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBQpqVRF1YLNUrPQsF5TgE5veAof_bru68', 
+    projectId: 'xrp-bounties',
+  }
 };
